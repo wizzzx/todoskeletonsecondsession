cb277c347d1408c871a594f4ca049874eae80ee3
wizzzx <alexkryukovzzx@gmail.com>
2024-07-07 12:33:56 +0300

    initial commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", () => {
+  renderKanban();
+
+  const addTaskButtons = document.querySelectorAll(".kanban__icon--add");
+  const modal = document.querySelector(".modal");
+  const closeModalButton = document.querySelector(".close-modal");
+  const cancelButton = document.querySelector(".cancel-button");
+  const form = document.querySelector(".modal form");
+
+  // =========================================================================
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById("title-task").value;
+    const description = document.getElementById("description-task").value;
+    const dueDate = form.elements["date"].value;
+
+    // Вставить код для создания задачи
+  });
+  // =========================================================================
+
+  addTaskButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      modal.style.display = "flex";
+    });
+  });
+
+  closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  cancelButton.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+});
+
+
+// =========================================================================
+function renderTaskElement() {
+  // Вставить код рендера задачи
+}
+// =========================================================================
+
+function renderKanban() {
+  const kanbanContainer = document.querySelector(".kanban");
+
+  columns.forEach((column) => {
+    const columnSection = document.createElement("section");
+    columnSection.className = `kanban__column kanban__column--${column.id}`;
+    columnSection.innerHTML = `
+          <div class="kanban__header">
+            <div class="kanban__header-content">
+              <img src="${column.icon}" alt="Колонка ${column.title}" class="kanban__icon kanban__icon--column">
+              <h2 class="kanban__title">${column.title}</h2>
+            </div>
+            <img src="./src/assets/img/kanban/plus.svg" alt="Добавить задачу" class="kanban__icon kanban__icon--add">
+          </div>
+          <div class="kanban__list"></div>
+        `;
+
+    const listContainer = columnSection.querySelector(".kanban__list");
+    column.tasks.forEach((taskId) => {
+      const task = tasks.find((task) => task.id === taskId);
+      if (task) {
+        // listContainer.appendChild(renderTaskElement(task));
+      }
+    });
+
+    kanbanContainer.appendChild(columnSection);
+  });
+}
---
+++ src/assets/js/list.js
@@ -0,0 +1,33 @@
+const columns = [
+    {
+      id: "to-do",
+      title: "Задачи",
+      icon: "./src/assets/img/kanban/kanban__column--to-do.svg",
+      tasks: []
+    },
+    {
+      id: "in-progress",
+      title: "В процессе",
+      icon: "./src/assets/img/kanban/kanban__column--in-progress.svg",
+      tasks: []
+    },
+    {
+      id: "done",
+      title: "Выполнено",
+      icon: "./src/assets/img/kanban/kanban__column--done.svg",
+      tasks: []
+    }
+  ];
+  
+
+  const tasks = [
+    {
+      id: 1,
+      title: "Задача 1",
+      description: "Описание задачи 1",
+      complexity: "high",
+      dueDate: "16.11.2023",
+      columnId: "to-do"
+    },
+  ];
+  
