abe2216bb5bc7ea236306102030c773d3d5b86e4
wizzzx <alexkryukovzzx@gmail.com>
2024-07-15 21:30:55 +0300

    cleaned commentary

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,6 @@
 
 let currentColumnId = null;
-// нужна чтобы пушить новые таски в массив, также для отображение задачи в нужной колонке
 document.addEventListener("DOMContentLoaded", () => {
-  // код будет работать только после загрузки всего документа
   renderKanban();
 
   const addTaskButtons = document.querySelectorAll(".kanban__icon--add");
@@ -11,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const cancelButton = document.querySelector(".cancel-button");
   const form = document.querySelector(".modal form");
 
-  // =========================================================================
   form.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -22,9 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const description = document.getElementById("description-task").value;
     const dueDate = form.elements["date"].value;
   
-    // Вставить код для создания задачи, тут ваще не пон, хуйня не гуглится (закомментированный код скорее всего неправильный)
     const newTask = {
-      // нужно как то генерить уникальный айдишник у новых задач, узнать
       id: 2,
       title: title,
       description:description,
@@ -34,28 +29,20 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
     tasks.push(newTask);
-    // пушим в массив новый объект
     const currentColumn = columns.find(col => col.id == currentColumnId);
     currentColumn.tasks.push(newTask.id)
-// ищем в массиве колумнс объекты по айдишникам
     modal.style.display = 'none';
-// закрыть окно после нажатия "submit"
     const currentColumnDomElement = document.querySelector(`.kanban__column--${currentColumnId}`);
-// не уврен что именно делает конструкция "--", допустим объединяет класс со свойством объекта массива
-// на самом деле она связывает класс и js код внутри, наверное?
     const kanbanListCurrentColumn = currentColumnDomElement.querySelector('.kanban__list')
     kanbanListCurrentColumn.appendChild(renderTaskElement(newTask))
 
     
   });
-  // =========================================================================
 
   addTaskButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
       const columnId = e.target.dataset.columnId;
-      // dataset считываем дата атрибут data-column-id
       currentColumnId = columnId;
-      //присваиваем переменной значение тут, а выполняется она выше
       modal.style.display = "flex";
     });
   });
@@ -70,40 +57,21 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-// =========================================================================
 function renderTaskElement(task) {
   const element = document.createElement('div');
   element.className = `task-item_info task-item_info--${task.id}`;
-  // узнать что именно делает эта конструкция
   element.innerHTML = `
     <div class="task-item">
         <div class="task-item__title">${task.title}</div>
         <div class="task-item__description">${task.description}</div>
         <div class="task-item__client">${task.dueDate}</div>
         <div class="complexity__dot"></div>
-    </div>
-  ` // по принципу рендер канбан (не совсем)
+    </div>`
   
   return element;
 };
-  // Вставить код рендера задачи (неправильно, но оставлю для истории)
-  // const taskContainer = document.querySelector(".task-item");
-
-  // tasks.forEach((task) => {
-  //   const taskCard = document.createElement("task");
-  //   taskCard.className = `task-item_info task-item_info--${task.id}`;
-  //   taskCard.innerHTML = `
-      // <div class="task-item">
-      //   <div class="task-item_title">${title}</div>
-      //   <div class="task-item_description">${description}</div>
-      //   <div class="task-item_client">${dueDate}</div>
-      // </div>
-  //   `;
-// =========================================================================
-
 
 function renderKanban() {
-  // функция вызывается при загрузке документа (выше)
   const kanbanContainer = document.querySelector(".kanban");
 
   columns.forEach((column) => {
@@ -117,21 +85,12 @@ function renderKanban() {
             </div>
             <img data-column-id="${column.id}" src="./src/assets/img/kanban/plus.svg" alt="Добавить задачу" class="kanban__icon kanban__icon--add">
           </div>
-          <div class="kanban__list"></div>
-        `; // добавил строчку сюда, привязал колумн айди к кнопке "добавить задачу", чтобы отображалось в правильной колонке
+          <div class="kanban__list"></div>`;
 
     const listContainer = columnSection.querySelector(".kanban__list");
 
-
-    // у колонки свойство tasks - это задачи колонки
-    // проходит по этому массиву - а исходя из того что элемент массива в исходной задаче называется taskId 
-    // должен был догадаться что свойство tasks - это ничто иное как массив айдишников
-
-    // ITER 1
-    // todo col -> tasks
     column.tasks.forEach((taskId) => {
       const task = tasks.find((task) => task.id === taskId);
-      //=== строгое сравнение, чтобы по типам совпадало
       if (task) {
         listContainer.appendChild(renderTaskElement(task));
       }
